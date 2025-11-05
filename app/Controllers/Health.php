6343cc67bc32df967f1625b7f334f3e54bcd14f4
Ubuntu <ubuntu@ip-172-26-0-150.ec2.internal>
2025-11-05 04:49:38 +0000

    error remove from health controller

--- app/Controllers/Health.php
+++ app/Controllers/Health.php
@@ -18,22 +18,3 @@ class Health extends ResourceController
         }
     }
 }
-<?php
-namespace App\Controllers;
-
-use CodeIgniter\RESTful\ResourceController;
-use App\Models\UserModel;
-
-class Health extends ResourceController
-{
-    public function db()
-    {
-        try {
-            $model = new UserModel();
-            $count = $model->countAllResults(); // quick operation
-            return $this->respond(['db_ok' => true, 'users_count' => $count]);
-        } catch (\Throwable $e) {
-            return $this->respond(['db_ok' => false, 'error' => $e->getMessage()], 500);
-        }
-    }
-}
